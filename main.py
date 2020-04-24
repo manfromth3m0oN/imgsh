3bf3ab3b7f66765a60a65d2e0d292859042fc92b
Max S-T <maxstantay@gmail.com>
2020-04-24 22:57:10 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, request, redirect
+import redis
+from werkzeug.utils import secure_filename
+import time
+
+app = Flask(__name__)
+db = redis.Redis(host='localhost', port=6379, db=0)
+
+current_milli_time = lambda: int(round(time.time() * 1000))
+
+@app.route('/')
+def index():
+    top = db.scan(count=9)[1]
+    return render_template('index.html', posts=top)
+
+@app.route('/<id>')
+def img(id):
+    time = db.get(id)
+    return render_template('img.html', id=id, time=time.decode())
+
+@app.route('/uupload')
+def uploadpage():
+    return render_template('upload.html')
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    file = request.files['img']
+    file.save('./static/storage/' + secure_filename(file.filename))
+    db.set(secure_filename(file.filename), current_milli_time())
+    return redirect('/')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
